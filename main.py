32aceb247a502474a415d5448db9d5e35bd5245e
techacct <ademolaoladapo@gmail.com>
2022-07-12 14:19:31 +0100

    instantiate instance

--- main.py
+++ main.py
@@ -1,14 +1,33 @@
 class Item:
+    pay_rate = 0.8 # price - discount of 20%
+    all = []
     def __init__(self, name: str, price: float, quantity=0):
         # run validations for arguments received
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
         assert quantity >= 0, f"Price {price} is not greater than or equal to zero!"
 
 
-        #Assign self to object
+        # assign self to object
         self.name = name
         self.price = price
         self.quantity = quantity
 
+        # action to execute
+        Item.all.append(self)
+
     def calculate_total_price(self):
         return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+    
+    def __repr__(self):
+        return 'Item'
+
+item1 = Item("Phone", 100, 1)
+item2 = Item("Laptop", 1000, 3)
+item3 = Item("Cable", 10, 5)
+item4 = Item("Mouse", 50, 5)
+item5 = Item("Keyboard", 75, 5)
+
+print(Item.all)
