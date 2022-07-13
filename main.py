b78365a85313bd3466acc02b8547cefbc616b736
techacct <ademolaoladapo@gmail.com>
2022-07-13 04:56:59 +0100

    add a method to read in data from csv

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
+import csv
+
 class Item:
     pay_rate = 0.8 # price - discount of 20%
     all = []
     def __init__(self, name: str, price: float, quantity=0):
         # run validations for arguments received
         assert price >= 0, f"Price {price} is not greater than or equal to zero!"
-        assert quantity >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Price {quantity} is not greater than or equal to zero!"
 
 
         # assign self to object
@@ -21,13 +23,21 @@ class Item:
     def apply_discount(self):
         self.price = self.price * self.pay_rate
     
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+    
     def __repr__(self):
         return f"Item('{self.name}', {self.price}, {self.quantity})"
 
-item1 = Item("Phone", 100, 1)
-item2 = Item("Laptop", 1000, 3)
-item3 = Item("Cable", 10, 5)
-item4 = Item("Mouse", 50, 5)
-item5 = Item("Keyboard", 75, 5)
-
+Item.instantiate_from_csv()
 print(Item.all)
